6afeeea570b78b1e93211f9d768557627d23bf13
Donald P. Labajo Jr <ecedonaldlabajojr@gmail.com>
2021-01-30 22:51:32 +0800

    Latest commit to github repository. Hid old repository (with previous commits for this coding exam), commit latest version after housekeeping, updating .gitignore file to avoid 'node_modules' from getting uploaded to github

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 
+// Database Configuration
 module.exports = {
     database: `mongodb+srv://admin-dondon28:${process.env.MONGODB_ATLAS_PW}@cluster0.loiov.mongodb.net/MedSpec_UsersDB`,
     dbOptions: {
--- util/message.js
+++ util/message.js
@@ -1,3 +1,6 @@
+// This function checks the message type (success or danger) in <request.session.messageType> attribute
+// and passes the returned value as a parameter to EJS template
+
 function checkMessageType(requestObject) {
     switch (requestObject.session.messageType) {
         case "danger":
